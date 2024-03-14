a660bc00a0bb89d0a01940d3e4c0abe6c7770d30
Caleb Villalba <calvihe@gmail.com>
2024-03-14 08:56:17 -0500

    try fl0

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ let dotenv = require("dotenv").config();
 
 exports.serverConfig = {
   databaseUri: dotenv.parsed.DATABASE_URI,
-  port: dotenv.parsed.BACKEND_PORT || 5000,
+  port: dotenv.parsed.PORT || 5000,
   secret: "avenge-sanautos-secret-key",
 };
