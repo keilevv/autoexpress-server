5e792eb7395f188716a9d32f84494ebe2e54fed0
Caleb Villalba <calvihe@gmail.com>
2024-04-05 11:27:00 -0500

    fix www file

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let express = require("express");
+var path = require('path');
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let app = express();
 let cors = require("cors");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
 
 let config = require("./config/");
-
-// Models
-
-const Role = require("./models/roleModel");
 
 // Configure bodyparser to handle post requests
 app.use(
@@ -16,8 +15,11 @@ app.use(
     extended: true,
   })
 );
+app.use(logger("dev"));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
@@ -28,42 +30,13 @@ mongoose
   .then(() => console.log("Db connected successfully"))
   .catch((err) => console.error("Error connecting db: ", err));
 
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-        console.log("added 'user' to roles collection");
-      });
-      new Role({
-        name: "moderator",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-        console.log("added 'moderator' to roles collection");
-      });
-      new Role({
-        name: "admin",
-      }).save((err) => {
-        if (err) {
-          console.log("error", err);
-        }
-        console.log("added 'admin' to roles collection");
-      });
-    }
-  });
-}
-
 // Setup server port
 var port = config.serverConfig.port;
 
 // Send message for default URL
-app.get("/", (req, res) => res.json("Hello World with Express"));
+app.get("/", (req, res) =>
+  res.json("Hello World with Express from autoexpress")
+);
 
 // Use Api routes in the App
 require("./routes/auth")(app);
@@ -77,3 +50,5 @@ require("./routes/message")(app);
 app.listen(port, function () {
   console.log("Running RestHub on port " + port);
 });
+
+module.exports = app;
