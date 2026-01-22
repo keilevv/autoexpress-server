05167cd4b7202580601445b0dc1023fee1bb530a
Caleb Villalba <calvihe@gmail.com>
2026-01-22 10:24:35 -0500

    update default port

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ let dotenv = require("dotenv").config();
 
 exports.serverConfig = {
   databaseUri: process.env.DATABASE_URI,
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3008,
   secret: "avenge-sanautos-secret-key",
   environment: process.env.NODE_ENV || "production",
 };
